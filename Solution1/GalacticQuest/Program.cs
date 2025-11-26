3d9ccce2ec3eb7a86b86c220877a35aef42d57de
denis-adelin <denis.bardas@student.unitbv.ro>
2025-11-26 17:45:19 +0200

    Work from workshop

---
+++ Solution1/GalacticQuest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GalacticQuest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGameRunning = true;
+
+            while (isGameRunning)
+            {
+                string input = Console.ReadLine();
+                int.TryParse(input, out int inputValue);
+                switch (inputValue)
+                {
+                    case (int)Menu.Monsters:
+                        Console.WriteLine("You selected Monsters.");
+                        break;
+                    case (int)Menu.Exit:
+                        Console.WriteLine("You choosed exit");
+                        isGameRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+        }
+    }
+}
